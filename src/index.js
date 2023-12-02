d251f73d34607099ee854354ba99718215335fc6
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-12-02 20:08:55 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// const firstElem = React.createElement("span", { children: "Hello" });
+// const secondElem = React.createElement("span", { children: "!!!" });
+
+const firstElem = <span>Hello</span>;
+const secondElem = <span>!!!</span>;
+
+// const element = React.createElement("div", {
+//     a: 5,
+//     b: 10,
+//     children: [firstElem, secondElem],
+// });
+
+const jsxElem = (
+    <div>
+        {firstElem}
+        {secondElem}
+        {2 + 2}
+    </div>
+);
+console.log(jsxElem);
+
+// ReactDOM.render(element, document.querySelector("#root"));
+
+// ============================================================================================
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
